697a337c6baccd1219342151cd733eba3400db17
iclectic <tansonestost@yahoo.com>
2024-01-04 01:45:45 +0000

    4.1.2024

--- src/App.js
+++ src/App.js
@@ -14,13 +14,52 @@ const candyColors = [
 const App= () => {
     const [currentColorArrangement, setCurrentColorArrangement] = useState([])
 
+    const checkForColumnOfFour = () => {
+        for (let i = 0; i < 39; i++ ) {
+            const columnOfFour = [ i, i +width, i + width * 2]
+            const decidedColor = currentColorArrangement[i]
+
+            if ( columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+                columnOfFour.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
+
+    const checkForRowOfTFour = () => {
+        for (let i = 0; i < 64; i++ ) {
+            const rowOfFour = [ i, i +1, i +2, i +3]
+            const decidedColor = currentColorArrangement[i]
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+            if (notValid.includes(i)) continue
+
+            if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+                rowOfFour.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
+
     const checkForColumnOfThree = () => {
         for (let i = 0; i < 47; i++ ) {
             const columnOfThree = [ i, i +width, i + width * 2]
             const decidedColor = currentColorArrangement[i]
 
             if ( columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+                columnOfThree.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
 
+    const checkForRowOfThree = () => {
+        for (let i = 0; i < 64; i++ ) {
+            const rowOfThree = [ i, i +1, i +2]
+            const decidedColor = currentColorArrangement[i]
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+            if (notValid.includes(i)) continue
+
+            if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+                rowOfThree.forEach(square => currentColorArrangement[square] = '')
             }
         }
     }
@@ -37,6 +76,16 @@ const App= () => {
     useEffect(() => {
         createBoard()
     }, [])
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            checkForColumnOfFour()
+            checkForColumnOfThree()
+            checkForRowOfThree()
+            setCurrentColorArrangement([...currentColorArrangement])
+        }, 100)
+        return () => clearInterval(timer)
+    }, [checkForColumnOfFour, checkForColumnOfThree, checkForRowOfThree, currentColorArrangement])
 
     console.log(currentColorArrangement)
 
