8fad261d20c6dc458e733c70cf65be75241c1650
iclectic <tansonestost@yahoo.com>
2024-01-06 19:05:12 +0000

    6.1.2024

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ const candyColors = [
 
 const App= () => {
     const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+    const [ squareBeingDragged, setSquareBeingDragged] = useState(null)
+    const [ squareBeingReplaced, setSquareBeingReplaced] = useState(null)
 
     const checkForColumnOfFour = () => {
-        for (let i = 0; i < 39; i++ ) {
-            const columnOfFour = [ i, i +width, i + width * 2]
+        for (let i = 0; i <= 39; i++ ) {
+            const columnOfFour = [ i, i +width, i + width * 2, i + width * 3]
             const decidedColor = currentColorArrangement[i]
 
             if ( columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
@@ -25,11 +27,11 @@ const App= () => {
         }
     }
 
-    const checkForRowOfTFour = () => {
+    const checkForRowOfFour = () => {
         for (let i = 0; i < 64; i++ ) {
             const rowOfFour = [ i, i +1, i +2, i +3]
             const decidedColor = currentColorArrangement[i]
-            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
 
             if (notValid.includes(i)) continue
 
@@ -40,7 +42,7 @@ const App= () => {
     }
 
     const checkForColumnOfThree = () => {
-        for (let i = 0; i < 47; i++ ) {
+        for (let i = 0; i <= 47; i++ ) {
             const columnOfThree = [ i, i +width, i + width * 2]
             const decidedColor = currentColorArrangement[i]
 
@@ -64,6 +66,51 @@ const App= () => {
         }
     }
 
+
+
+    const moveIntoSquareBelow = () => {
+
+       for (let i = 0; i <= 55; i++) {
+           const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+           const isFirstRow = firstRow.includes(i)
+
+           if (isFirstRow && currentColorArrangement[i] === '') {
+               let randomNumber = Math.floor(Math.random() * candyColors.length)
+               currentColorArrangement[i] = candyColors[randomNumber]
+           }
+
+           if ((currentColorArrangement[i + width]) === '') {
+                currentColorArrangement[i + width] = currentColorArrangement[i]
+                currentColorArrangement[i] = ''
+           }
+       }
+    }
+
+
+    const dragStart = (e) => {
+        console.log(e.target)
+        console.log('drag start')
+        setSquareBeingDragged(e.target)
+    }
+    const dragDrop = (e) => {
+        console.log(e.target)
+        console.log('drag drop')
+        setSquareBeingReplaced(e.target)
+    }
+    const dragEnd = (e) => {
+       console.log('drag end')
+
+        const squareBeingDraggedId = parseInt(squareBeingDragged.getAttribute('data-id'))
+        const squareBeingReplacedId = parseInt(squareBeingReplaced.getAttribute('data-id'))
+
+        currentColorArrangement[squareBeingReplacedId] = squareBeingDragged.style.backgroundColor
+        currentColorArrangement[squareBeingDraggedId] = squareBeingReplaced.style.backgroundColor
+
+        console.log('squareBeingDraggedId', squareBeingDraggedId)
+        console.log('squareBeingReplacedId', squareBeingReplacedId)
+    }
+
+
     const createBoard = () => {
         const randomColorArrangement = []
         for (let i = 0; i < width * width; i++) {
@@ -80,14 +127,16 @@ const App= () => {
     useEffect(() => {
         const timer = setInterval(() => {
             checkForColumnOfFour()
+            checkForRowOfFour()
             checkForColumnOfThree()
             checkForRowOfThree()
+            moveIntoSquareBelow()
             setCurrentColorArrangement([...currentColorArrangement])
-        }, 100)
+        }, 1000)
         return () => clearInterval(timer)
-    }, [checkForColumnOfFour, checkForColumnOfThree, checkForRowOfThree, currentColorArrangement])
+    }, [checkForColumnOfFour, checkForRowOfFour, checkForColumnOfThree, checkForRowOfThree, moveIntoSquareBelow, currentColorArrangement])
 
-    console.log(currentColorArrangement)
+
 
   return (
     <div className = "app">
@@ -97,6 +146,15 @@ const App= () => {
                  key={index}
                  style={{backgroundColor: candyColor}}
                  alt={candyColor}
+                 data-id={index}
+                 draggable={true}
+                 onDragStart={dragStart}
+                 onDragOver = {(e) => e.preventDefault()}
+                 onDragEnter = {(e) => e.preventDefault()}
+                 onDragLeave = {(e) => e.preventDefault()}
+                 onDrop={dragDrop}
+                 onDragEnd={dragEnd}
+
                  />
              ))}
          </div>
